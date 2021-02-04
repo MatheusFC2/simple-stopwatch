b6987554206462063f85d01b0794e07e297364aa
Matheus Campos <matheusfreitascampos@gmail.com>
2021-02-04 15:22:59 -0300

    Fix the erro, don't start de cron

--- script.js
+++ script.js
@@ -4,19 +4,21 @@ var hh = 0;
 var mm = 0;
 var ss = 0;
 
-var tempo = 1000;//Quantos milesimos tem um segundo?
+var tempo = 1000;//Quantos milésimos valem 1 segundo?
 var cron;
 
-function start(){
-    
-    cron = setInterval(() => {timer();}, tempo);
+//Inicia o temporizador
+function start() {
+    cron = setInterval(() => { timer(); }, tempo);
 }
 
-function pause(){
+//Para o temporizador mas não limpa as variáveis
+function pause() {
     clearInterval(cron);
 }
 
-function stop(){
+//Para o temporizador e limpa as variáveis
+function stop() {
     clearInterval(cron);
     hh = 0;
     mm = 0;
@@ -24,20 +26,27 @@ function stop(){
 
     document.getElementById('counter').innerText = '00:00:00';
 }
-function timer(){
 
-    ss++;
+//Faz a contagem do tempo e exibição
+function timer() {
+    ss++; //Incrementa +1 na variável ss
 
-    if(ss == 60){
-        ss = 0;
-        mm++;
-        if(mm == 60){
-            mm = 0;
-            hh++;
+    if (ss == 59) { //Verifica se deu 59 segundos
+        ss = 0; //Volta os segundos para 0
+        mm++; //Adiciona +1 na variável mm
+
+        if (mm == 59) { //Verifica se deu 59 minutos
+            mm = 0;//Volta os minutos para 0
+            hh++;//Adiciona +1 na variável hora
         }
     }
 
-    var format = (hh < 10 ? '0' + hh + hh) + ':' + (mm < 10 ? '0'+ mm : mm) + ':' + (ss < 10 ? '0' + ss : ss);
+    //Cria uma variável com o valor tratado HH:MM:SS
+    var format = (hh < 10 ? '0' + hh : hh) + ':' + (mm < 10 ? '0' + mm : mm) + ':' + (ss < 10 ? '0' + ss : ss);
+    
+    //Insere o valor tratado no elemento counter
     document.getElementById('counter').innerText = format;
-}
 
+    //Retorna o valor tratado
+    return format;
+}
